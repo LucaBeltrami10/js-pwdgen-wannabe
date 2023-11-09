aaaa253e73381a820faeb92775744db1eb73f2b3
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-09 14:49:32 +0100

    modif. valore della variabile let yourNewPassword

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-const askName="Inserisci il tuo nome";
-const askSurname="Inserisci il tuo cognome";
-const askColor="inserisci il tuo colore preferito";
+const askName = "Inserisci il tuo nome";
+const askSurname = "Inserisci il tuo cognome";
+const askColor = "inserisci il tuo colore preferito";
 
-let yourNewPassword="----"
+let yourNewPassword
 
-const selectedName=parseInt(prompt(askName))
-const selectedSurname=parseInt(prompt(askSurname))
-const selectedColor=parseInt(prompt(askColor))
+
+
+const selectedName = parseInt(prompt(askName))
+const selectedSurname = parseInt(prompt(askSurname))
+const selectedColor = parseInt(prompt(askColor))
+
+yourNewPassword = selectedName + selectedSurname + selectedColor
+
+console.log(yourNewPassword)
